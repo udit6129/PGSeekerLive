d6f57161b7928b683c58ade7a17db7c76e674307
uditjain <uditjain2622004@gmail.com>
2023-08-01 21:45:49 +0530

    Minor changes

--- controllers/authController.js
+++ controllers/authController.js
@@ -23,10 +23,11 @@ const createSendToken = (user, statusCode, req, res) => {
       expires: new Date(
         Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000
       ),
-      // domain: "*",
+      domain: "pgseeker.netlify.app",
       // domain: "localhost",
       httpOnly: true, //  with "httpOnly :true" browser can not modify our cookie, it can just recieve it
       path: "/",
+      secure: true,
     };
 
     if (req.secure || req.headers["x-forwarded-proto"] === "https")
